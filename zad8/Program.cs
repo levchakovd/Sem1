c4d263b27cef019d93ed7403bef98adba646a94f
levchakovd <levchakovd@yandex.ru>
2022-07-13 22:57:05 +0300

    zad8

---
+++ zad8/Program.cs
@@ -0,0 +1,11 @@
+﻿int a,b;
+Console.WriteLine("Введи первое число - ");
+a = Convert.ToInt32(Console.ReadLine());
+b=1;
+while (b<=a)
+{
+    if (b%2==0)
+    Console.WriteLine(b++);
+    else
+    b++;
+}
