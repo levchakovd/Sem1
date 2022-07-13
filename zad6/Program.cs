f26fb6a5e8d7b7c0730b59f20af75db865959d51
levchakovd <levchakovd@yandex.ru>
2022-07-13 22:14:44 +0300

    zad6

---
+++ zad6/Program.cs
@@ -0,0 +1,11 @@
+﻿int a;
+Console.WriteLine("Введи Ваше число - ");
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
