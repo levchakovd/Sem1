562662096d752c96350cb27d045880c2fdcfffc8
levchakovd <levchakovd@yandex.ru>
2022-07-13 21:47:21 +0300

    2 zad

---
+++ zad2/Program.cs
@@ -0,0 +1,14 @@
+﻿int a, b;
+Console.WriteLine("Введи первое число - ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число - ");
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("max ="+ a);
+}
+else
+{
+    Console.WriteLine("max ="+ b);
+}
+
