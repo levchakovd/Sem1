f6e31ea4a5258ea8dbe79fded227480ed2c68d62
levchakovd <levchakovd@yandex.ru>
2022-07-13 22:01:15 +0300

    zad4

---
+++ zad4/Program.cs
@@ -0,0 +1,11 @@
+﻿int a,b,c;
+Console.WriteLine("Введи первое число - ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число - ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи третье число - ");
+c = Convert.ToInt32(Console.ReadLine());
+int []  numbers = {a,b,c};
+int MaxValue = numbers.Max();
+Console.WriteLine(MaxValue);
+
